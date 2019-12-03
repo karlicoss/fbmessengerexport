c35c3a53e812a10e8e1a42069bc9aad2c7bb1525
Dima Gerasimov <karlicoss@gmail.com>
2019-12-03 23:08:26 +0000

    Export to proper database

--- fbchatexport.py
+++ fbchatexport.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 import argparse
+from pathlib import Path
 import json
 import logging
+import sys
 from typing import List, Iterator, Union, TypeVar, Optional
+
+import dataset # type: ignore
 
 import fbchat # type: ignore
 from fbchat import Client, Thread, Message, ThreadLocation
@@ -14,6 +18,44 @@ Res = Union[T, Exception]
 def get_logger():
     return logging.getLogger('fbchatexport')
 
+
+def delk(d, key: str):
+    if key in d:
+        del d[key]
+
+class ExportDb:
+    def __init__(self, db_path: Path) -> None:
+        self.db = dataset.connect('sqlite:///{}'.format(db_path))
+        # TODO need to disconnect??
+        self.ttable = self.db['threads']
+        self.mtable = self.db['messages']
+
+    def insert_thread(self, thread: Thread) -> None:
+        dd = vars(thread)
+        delk(dd, 'type') # user vs group? fine without it for now
+        # TODO could remove remaining crap, e.g. color/emoji/plan, but for now don't bother
+
+        # TODO FIMXE pkey/upsert??
+        self.ttable.insert(dd)
+
+    def insert_message(self, message: Message) -> None:
+        dd = vars(message)
+        # TODO FIMXE just store as sqlite json??
+        # delete lists, not sure how to handle
+        delk(dd, 'mentions')
+        delk(dd, 'read_by')
+        delk(dd, 'attachments') # TODO not sure, what if urls??
+        delk(dd, 'quick_replies')
+        delk(dd, 'reactions')
+
+        delk(dd, 'replied_to') # we've got reply_to_id anyway
+
+        self.mtable.insert(dd)
+
+
+# doc doesn't say anything about cap and default is 20. for me 100 seems to work too
+# https://fbchat.readthedocs.io/en/stable/api.html#fbchat.Client.fetchThreadMessages
+FETCH_THREAD_MESSAGES_LIMIT = 100
 
 def iter_thread(client: Client, thread: Thread) -> Iterator[Res[Message]]:
     """
@@ -33,7 +75,7 @@ def iter_thread(client: Client, thread: Thread) -> Iterator[Res[Message]]:
         logger.debug('thread %s: fetching %d/%d', tname, done, limit)
 
         before = last_ts if last_msg is None else last_msg.timestamp
-        chunk = client.fetchThreadMessages(tid, before=before)
+        chunk = client.fetchThreadMessages(tid, before=before, limit=FETCH_THREAD_MESSAGES_LIMIT)
         if len(chunk) == 0:
             # not sure if can happen??
             yield RuntimeError("Expected non-empty chunk")
@@ -48,7 +90,9 @@ def iter_thread(client: Client, thread: Thread) -> Iterator[Res[Message]]:
         last_msg = chunk[-1]
 
 
-def iter_all(client: Client):
+# TODO maybe a json for each user? I guess it's ok to start with..
+# TODO atomic writes?
+def process_all(client: Client, db: ExportDb) -> Iterator[Exception]:
     logger = get_logger()
     # TODO what is pending??
     locs = [ThreadLocation.INBOX, ThreadLocation.ARCHIVED]
@@ -62,56 +106,65 @@ def iter_all(client: Client):
         threads.extend(thr)
 
     # TODO save threads as well?
+    # vars(thread)
+    # vars(msg?)
 
     # TODO def should be defensive...
-    threads = threads[:1] + threads[2:3] # TODO FIXME 
+    # threads = threads[:1] + threads[2:3] # TODO FIXME 
+    # threads = threads[:1] # TODO FIXME
     for thread in threads:
-        messages: List[Message] = []
+        db.insert_thread(thread)
+
         for r in iter_thread(client=client, thread=thread):
             if isinstance(r, Exception):
+                logger.exception(r)
                 yield r
             else:
-                messages.append(r)
+                # TODO WAL?
+                try:
+                    db.insert_message(r)
+                except Exception as e:
+                    logger.exception(e)
+                    # TODO FIXME
+                    from IPython import embed; embed()
 
-    # from IPython import embed; embed()
 
-def run(cookies: str):
+def run(*, cookies: str, db_path: Path):
+    logger = get_logger()
     client = Client(
         'dummy_email',
         'dummy_password',
         session_cookies=json.loads(cookies),
     )
-    list(iter_all(client=client)) # FIXME force it
+
+    db = ExportDb(db_path)
+
+    errors = list(process_all(client=client, db=db))
+    if len(errors) > 0:
+        logger.error('Had errors during processing')
+        sys.exit(1)
+
 
 def main():
     logger = get_logger()
     from kython.klogging import setup_logzero
-    setup_logzero(logger, level=logging.DEBUG)
+    setup_logzero(logger, level=logging.DEBUG) # TODO FIXME remove
     # logging.basicConfig(level=logging.INFO)
 
     # TODO move setup_logger there as well?
     from export_helper import setup_parser
     parser = argparse.ArgumentParser("Tool to export your personal Facebook chat/Messenger data")
     setup_parser(parser=parser, params=['cookies'])
+    parser.add_argument('--db-path', type=Path, help='Path to result sqlite database with')
     args = parser.parse_args()
 
     params = args.params
-    dumper = args.dumper
 
-    # TODO FIXME not sure how feasible is json for people who have LOTS of messages
-    # but that requires a whole different, incremental approach (e.g. like Telegram exporter)
-    # j = get_json(**params)
-    # js = json.dumps(j, ensure_ascii=False, indent=1)
-    # dumper(js)
-
-    run(cookies=params['cookies'])
+    run(cookies=params['cookies'], db_path=args.db_path)
 
 if __name__ == '__main__':
     main()
 
+
 # TODO mention: asks for 2FA
 # TODO shit, asks for 2FA again. wonder if can preserve session??
-
-# TODO ok, so this fetches only top 20
-# need a way to export all?
-# https://fbchat.readthedocs.io/en/stable/examples.html#examples
