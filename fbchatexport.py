e38c12626897342c9d6b769d7699b06c6fb08712
Dima Gerasimov <karlicoss@gmail.com>
2019-12-03 21:59:03 +0000

    login via cookies

--- fbchatexport.py
+++ fbchatexport.py
@@ -1,16 +1,40 @@
 #!/usr/bin/env python3
+import argparse
+import json
 
 import fbchat # type: ignore
 from fbchat import Client
 
-def run():
-    client = Client(email, password)
+
+def run(cookies: str):
+    client = Client(
+        'dummy_email',
+        'dummy_password',
+        session_cookies=json.loads(cookies),
+    )
+    threads = client.fetchThreadList()
+    print(threads)
+
 
 def main():
-    run()
+    from export_helper import setup_parser
+    parser = argparse.ArgumentParser("Tool to export your personal Facebook chat/Messenger data")
+    setup_parser(parser=parser, params=['cookies'])
+    args = parser.parse_args()
+
+    params = args.params
+    dumper = args.dumper
+
+    # TODO FIXME not sure how feasible is json for people who have LOTS of messages
+    # but that requires a whole different, incremental approach (e.g. like Telegram exporter)
+    # j = get_json(**params)
+    # js = json.dumps(j, ensure_ascii=False, indent=1)
+    # dumper(js)
+
+    run(cookies=params['cookies'])
 
 if __name__ == '__main__':
-    pass
+    main()
 
 # TODO mention: asks for 2FA
 # TODO shit, asks for 2FA again. wonder if can preserve session??
@@ -18,4 +42,3 @@ if __name__ == '__main__':
 # TODO ok, so this fetches only top 20
 # need a way to export all?
 # https://fbchat.readthedocs.io/en/stable/examples.html#examples
-# threads = client.fetchThreadList()
