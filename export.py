ff000ac5edaf25441f5e7763538de4e1d530c1d6
Dima Gerasimov <karlicoss@gmail.com>
2020-10-25 03:47:07 +0000

    fix login error (see https://github.com/fbchat-dev/fbchat/issues/615 )

--- export.py
+++ export.py
@@ -11,6 +11,11 @@ from typing import List, Iterator, Union, TypeVar, Optional, Tuple
 import dataset # type: ignore
 
 import fbchat # type: ignore
+### see https://github.com/fbchat-dev/fbchat/issues/615#issuecomment-710127001 
+import re
+fbchat._util.USER_AGENTS    = ["Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"]
+fbchat._state.FB_DTSG_REGEX = re.compile(r'"name":"fb_dtsg","value":"(.*?)"')
+###
 from fbchat import Client, Thread, Message, ThreadLocation
 
 
